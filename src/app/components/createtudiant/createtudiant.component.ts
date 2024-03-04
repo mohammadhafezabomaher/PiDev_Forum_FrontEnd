43f46cec65c7ebede72817aebcb97f00c93a8897
molkajlassi1 <124531449+molkajlassi1@users.noreply.github.com>
2024-03-04 20:21:57 +0100

    last

--- src/app/components/createtudiant/createtudiant.component.ts
+++ src/app/components/createtudiant/createtudiant.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NiveauSpecialite } from 'src/app/models/niveauspecialite';
 import { EtudiantService } from 'src/app/services/etudiant.service';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-createtudiant',
@@ -10,34 +12,47 @@ import { EtudiantService } from 'src/app/services/etudiant.service';
 export class CreatetudiantComponent implements OnInit {
   createForm!: FormGroup;
   imageUrl: string = 'assets/img/logo.jpg';
-  constructor(private httpService: EtudiantService, private fb: FormBuilder) { }
+  niveauSpecialites: NiveauSpecialite[] = [];
+
+  constructor(private httpService: EtudiantService,private nv:HttpService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.createForm = this.fb.group({
-      
-      faculte: [''],
-      typeEtudiant: [''],
+      faculte: ['', Validators.required],
+      typeEtudiant: ['', Validators.required],
       contact: this.fb.group({
-        nom: [''],
-        prenom: [''],
-        cin: [''],
-        email: [''],
-        password: [''],
-        dateNaissance: [''],
-        telephone: [''],
-        adresse: [''],
-        
+        nom: ['', Validators.required],
+        prenom: ['', Validators.required],
+        cin: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        dateNaissance: ['', Validators.required],
+        telephone: ['', Validators.required],
+        adresse: ['', Validators.required],
       })
     });
+
+
+    this.nv.getAllNiveauSpecialites().subscribe(
+      (niveauSpecialites) => {
+        this.niveauSpecialites = niveauSpecialites;
+      },
+      (error) => {
+        console.log('Error fetching NiveauSpecialites:', error);
+      }
+    );
+
+
+
   }
 
+ 
   handelSubmit(): void {
     if (this.createForm.valid) {
       this.httpService.createEtudiant(this.createForm.value).subscribe(
         (response) => {
           console.log('Success:', response);
           this.createForm.reset();
-          
         },
         (error) => {
           console.log('Error:', error);
@@ -46,4 +61,9 @@ export class CreatetudiantComponent implements OnInit {
     }
   }
 
+  // Helper method to access form controls and check their validity
+  isControlInvalid(controlName: string): boolean {
+    const control = this.createForm.get(controlName);
+    return control !== null && control.invalid && control.touched;
+  }
 }
--- src/shared/sidebar/sidebar.component.ts
+++ src/shared/sidebar/sidebar.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  imageUrl: string = 'assets/img/logo.jpg';
 }
